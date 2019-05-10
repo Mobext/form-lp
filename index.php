5740adced26d3be8ceaab6536594f437799a78a2
alejomann <alejo.mann@gmail.com>
2019-05-10 15:41:59 -0300

    sas
    as

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Citroen</title>
+    <title>Citroen2</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     
     <link rel="stylesheet" type="text/css" href="css/normalize.css" />
